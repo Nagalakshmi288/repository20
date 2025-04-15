5f56491336a92c5a050149ed7c8544b6653b0ff1
Nagalakshmi288 <lakshmibattinavenkata@gmail.com>
2025-04-15 16:45:13 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,10 +1,16 @@
 resource "azurerm_resource_group""name" {
     name = "rg01"
     location="eastus"
+    tags = {
+      owner = "IT"
+   }
 
 }
 resource "azurerm_resource_group""name" {
     name = "rg02"
     location="westus"
+    tags = {
+      owner = "dev"
+   }
 
 }
